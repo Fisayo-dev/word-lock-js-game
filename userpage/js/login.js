368d29f3c48fe559c5875c9ff9f9b9f9b13aec23
Obadina Fisayo <olufisayobadina@gmail.com>
2023-04-30 21:56:14 +0100

    App Updated

--- userpage/js/login.js
+++ userpage/js/login.js
@@ -6,6 +6,10 @@ function collect(className) {
     return document.querySelector(`.${className}`)
 }
 
+function grab(element) {
+    return document.querySelector(`${element}`)
+}
+
 const form = collect('form')
 const page = collect('page')
 
@@ -87,7 +91,7 @@ class Form{
         .filter(user => user.type == 'user')
         .forEach(user => {
             if(user.email == userMail && user.pwd == userPwd) {
-                alert(`${user.email} has been logged in`)
+                showAlert('Hurray',`${user.email} has been logged In`,'success')
                 showUserPage(user.fname,user.lname,user.country,user.age)
                 Friend.displayFriend(users)
                 showUsers(users)
@@ -95,8 +99,34 @@ class Form{
             }
         })
         
+        
+    }
+    static checkUserExist(userMail,userPwd) {
+        const users = JSON.parse(localStorage.getItem('fruit-app'))
 
+        const arrUser = users.filter(user => user.type == 'user').filter(user => user.email == userMail.toString() && user.pwd == userPwd)
+        if(arrUser.length > 0) {
+            return true
+        } else {
+            return false
+        }
 
+    }
+
+    static checkUserPwdValid(userMail,userPwd) {
+        const users = JSON.parse(localStorage.getItem('fruit-app'))
+
+        const arrUsers = users
+        .filter(user => user.type == 'user')
+        .filter(user => user.email == userMail)
+        .flat(2)
+        // arrUsers.map(user => {
+        if(arrUsers.pwd !== userPwd) {
+            return false
+        }
+        // })
+
+        
     }
 }
 
@@ -182,19 +212,89 @@ function showUsers(list){
 
 loginBtn = form.querySelector('#login-btn')
 
-loginBtn.addEventListener('click', function(e) {
-    e.preventDefault()
-    if(email.value == '') {
-        console.log('Error: Email not specified');
+function showAlert(title,msg,type) {
+    const page = grab('body')
+    page.classList.add('dark-scene')
+    // if(title === 'You \'re finished') {
+    //     score = 0
+    //     index = 0
+    //     homePage.classList.remove('none')
+    //     gamePage.classList.add('none')
+    // }
+
+    const div = document.createElement('div')
+    div.classList.add('msg-box')
+    const value = `
+        <img src="#">
+        <h2>${title}</h2>
+        <p>${msg}</p>
+        <button class="btn">Got It!</button>
+
+    `
+    div.innerHTML = value
+
+    // const msgTitle = msgBox.querySelector('h2') 
+    // const msgCont = msgBox.querySelector('p') 
+    const msgImg = div.querySelector('img') 
+
+
+    // msgTitle.innerHTML = title;
+    // msgCont.innerHTML = msg;
+
+    switch(type){
+        case 'error': 
+            msgImg.setAttribute('src','../img/warn-icon.png') 
+        break;
+
+        case 'success': 
+            msgImg.setAttribute('src','../img/check-icon.png') 
+            msgImg.setAttribute('id', 'no-rotate')
+            break;
+            
+        case 'info':
+            msgImg.setAttribute('src','../img/Game logo 2.png') 
+        break;
+
+        default: 
+            msgImg.setAttribute('src','../img/logo 5.png') 
+        break;
     }
 
-    if(password.value == '') {
-        console.log('Error: Password not specified');
+
+    const body = document.querySelector('body')
+    body.appendChild(div)
+
+
+    // Clicked gotIt button
+    const gotItBtn = div.querySelector('.btn')
+    gotItBtn.addEventListener('click', function()  {
+        gotItBtn.parentElement.remove()
+        page.classList.remove('dark-scene')
+    })
+   
+}
+
+loginBtn.addEventListener('click', function(e) {
+    e.preventDefault()
+    if(email.value.trim() == '' || password.value.trim() == '') {
+        showAlert('Error','Fields Cannot be empty','info')
     }
 
     if(password.value !== '' && email.value !== ''){
-        Form.loginUser(email.value,password.value)
-        clearFields(password,email)
+        const emailVal = email.value
+        const passwordVal = password.value
+        
+        if(Form.checkUserExist(emailVal,passwordVal)) {
+            Form.loginUser(email.value,password.value)
+            clearFields(password,email)
+        } 
+         else {
+            showAlert('Wait','The details specified don\'t exist in our database')
+        }
+        // else {
+        //     showAlert('Sorry','The Email provided does not exist in our database, try logging in', 'error')
+        // }
+        
     }
 })
 
